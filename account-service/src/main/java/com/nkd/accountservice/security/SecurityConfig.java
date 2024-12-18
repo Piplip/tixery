a01cbe592e66b25e3b6b4307a7cb1f2d8411f4fd
Piplip <93427122+Piplip@users.noreply.github.com>
2024-12-18 21:08:47 +0700

    adding security configuration

---
+++ account-service/src/main/java/com/nkd/accountservice/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.nkd.accountservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request ->
+                request
+                    .requestMatchers("/sign-up", "/login").permitAll()
+                    .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public CustomUserDetailService customUserDetailService(){
+        return new CustomUserDetailService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+}
