4c0326ba289c7ca23cfb4f3069a9272f26b38fca
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-02 22:18:54 +0700

    adding role name for authorization

--- account-service/src/main/java/com/nkd/accountservice/security/CustomUserDetailService.java
+++ account-service/src/main/java/com/nkd/accountservice/security/CustomUserDetailService.java
@@ -1,11 +1,11 @@
 package com.nkd.accountservice.security;
 
+import com.nkd.accountservice.enums.RoleRoleName;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jooq.*;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import com.nkd.accountservice.security.CustomUserDetails;
 
 import static com.nkd.accountservice.Tables.*;
 
@@ -17,7 +17,7 @@ public class CustomUserDetailService implements UserDetailsService {
 
     @Override
     public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        var data = context.select(USER_ACCOUNT.ACCOUNT_EMAIL, CREDENTIAL.PASSWORD, ROLE.ROLE_PRIVILEGES)
+        var data = context.select(USER_ACCOUNT.ACCOUNT_EMAIL, CREDENTIAL.PASSWORD, ROLE.ROLE_PRIVILEGES, ROLE.ROLE_NAME)
                 .from(USER_ACCOUNT.leftJoin(CREDENTIAL).on(USER_ACCOUNT.CREDENTIAL_ID.eq(CREDENTIAL.CREDENTIAL_ID))
                         .leftJoin(ROLE).on(USER_ACCOUNT.ROLE_ID.eq(ROLE.ROLE_ID)))
                 .where(USER_ACCOUNT.ACCOUNT_EMAIL.eq(email))
@@ -28,12 +28,12 @@ public class CustomUserDetailService implements UserDetailsService {
             return null;
         }
 
-        Record3<String, String, String> record = data.get();
+        Record4<String, String, String, RoleRoleName> record = data.get();
 
         return CustomUserDetails.builder()
                 .username(record.component1())
                 .password(record.component2())
-                .rolePrivileges(record.component3())
+                .rolePrivileges(record.component3() + ",ROLE_" + record.component4().name())
                 .build();
     }
 
