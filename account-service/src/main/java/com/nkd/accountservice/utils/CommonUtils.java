3ab69cff6ff429a07d0e7d9911bd76719593fd03
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-04 00:00:23 +0700

    adding forgot password function for non-oauth2 user

--- account-service/src/main/java/com/nkd/accountservice/utils/CommonUtils.java
+++ account-service/src/main/java/com/nkd/accountservice/utils/CommonUtils.java
@@ -11,4 +11,14 @@ public class CommonUtils {
         }
         return builder.toString();
     }
+
+    public static String generateRandomNumber(int length) {
+        String NUMERIC_STRING = "0123456789";
+        StringBuilder builder = new StringBuilder();
+        while (length-- != 0) {
+            int character = (int)(Math.random()*NUMERIC_STRING.length());
+            builder.append(NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString();
+    }
 }
