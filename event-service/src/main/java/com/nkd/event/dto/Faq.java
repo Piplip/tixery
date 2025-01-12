67c7864bbc72feddd15561774a24f769e20f7e73
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-13 00:40:01 +0700

    fixing lombok

---
+++ event-service/src/main/java/com/nkd/event/dto/Faq.java
@@ -0,0 +1,14 @@
+package com.nkd.event.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Faq {
+
+    private String question;
+    private String answer;
+}
