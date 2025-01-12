88a14b18abc367a9c4c5d1fef3d84fa091dc27ec
Piplip <93427122+Piplip@users.noreply.github.com>
2025-01-13 00:41:44 +0700

    event service init

---
+++ event-service/src/test/java/com/nkd/event/EventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkd.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
